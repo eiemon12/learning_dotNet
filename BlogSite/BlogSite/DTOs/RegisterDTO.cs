b50527464fe542ead95983dae339025a591edb02
eiemon12 <ei.emon@yahoo.com>
2024-06-30 15:13:12 +0600

    working labtask

---
+++ BlogSite/BlogSite/DTOs/RegisterDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BlogSite.DTOs
+{
+    public class RegisterDTO
+    {
+        public int UId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public int Phone { get; set; }
+        [Required]
+        public string Dob { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
